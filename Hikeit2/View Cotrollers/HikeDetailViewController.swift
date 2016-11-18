2475e8dc0341cecc062990f51a1aed3be59fae0e
ctrador <chad.trador@interapthq.com>
2016-11-18 00:07:09 -0500

    feature Slider

--- Hikeit2/View Cotrollers/HikeDetailViewController.swift
+++ Hikeit2/View Cotrollers/HikeDetailViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class HikeDetailViewController: UIViewController {
+    
 
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var HikeDifficultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +27,25 @@ class HikeDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func sliderAction(_ sender: Any) {
+    
+
+        let easy = "Walk In The Park"
+        let medium = "That Was a Nice Workout"
+        let hard = "That was No Walk In The Park"
+        switch slider.value {
+        case 0...1.5:
+            HikeDifficultLabel.text = easy
+        case 1.6...2.5:
+            HikeDifficultLabel.text = medium
+        case 2.6...3:
+            HikeDifficultLabel.text = hard
+        default:
+            return
+        }
+    }
+       //HikeDifficultLabel.text = String(slider.value)
+    //}
 
     /*
     // MARK: - Navigation
